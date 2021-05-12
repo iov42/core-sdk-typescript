1af822653548319e38751008b7e4d92b9f760811
luiziov42 <44193194+luiziov42@users.noreply.github.com>
2021-05-12 10:29:40 +0200

    Added signature verification (#8)

---
+++ tests/signature.ts
@@ -0,0 +1,37 @@
+import { expect } from "chai";
+import "mocha";
+import { v4 as uuidv4 } from "uuid";
+import { IKeyPairData, PlatformUtils, ProtocolIdType } from "../iov42/core-sdk";
+
+const platformUtils = new PlatformUtils();
+
+let keyPair: IKeyPairData;
+
+const inputs = [
+    {
+        protocolId: "SHA256WithRSA" as ProtocolIdType,
+    },
+    {
+        protocolId: "SHA256WithECDSA" as ProtocolIdType,
+    },
+];
+
+inputs.forEach(function(input) {
+    describe(`Testing create keypair method with protocolId="${input.protocolId}"`, function() {
+        this.timeout(60000);
+
+        before('Initializing keypair', () => {
+            keyPair = platformUtils.generateKeypairWithProtocolId(input.protocolId, uuidv4());
+        });
+
+        describe("Test signature", function() {
+            it("should return success", async function() {
+                const payload = "This is a simple string"
+                const signature = await platformUtils.signWithProtocolId(keyPair.protocolId, keyPair.prvKeyBase64, payload)
+                const verify = await platformUtils.verifyWithProtocolId(keyPair.protocolId, keyPair.pubKeyBase64, payload, signature)
+                expect(verify).to.equal(true)
+            });
+        });
+    });
+
+});
